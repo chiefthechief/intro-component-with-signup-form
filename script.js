09e57db4b3d50329a13c6488c3187d273793828f
cujo <cujothechief@gmail.com>
2022-01-14 00:47:17 +0000

    further styling of the html file

---
+++ script.js
@@ -0,0 +1,7 @@
+let backgroundImage = document.querySelector('body');
+let firstName = document.querySelector('#main-right #section form input:nth-child(1)');
+let lastName = document.querySelector('#main-right #section form input:nth-child(4)');
+let emailAddress = document.querySelector('#main-right #section form input:nth-child(7)');
+let password = document.querySelector('#main-right #section form input:nth-child(10)');
+let submitButton = document.querySelector('#main-right #section form input[type = submit]');
+console.log(firstName, lastName, emailAddress, password)
